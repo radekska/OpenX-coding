d96109b9bc2678c18e6201d04ff79a7155e81961
rska <bp4fradek@gmail.com>
2020-03-07 14:03:39 +0100

    main.py modified

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from source import *
 
-data_json = get_data()
 
-user_post_iterator = UsersnPosts(data_json[0], data_json[1])
+if __name__ == '__main__':
+	data_json = get_data()
 
-print("============= 1 - LICZBA POSTÓW ===================")
-print(count_posts(user_post_iterator), end='\n\n')
+	user_post_iterator = UsersnPosts(data_json[0], data_json[1])
 
-print("=========== 2 - NIEUNIKALNE TYTUŁY POSTÓW ============")
-print(check_unique_title(user_post_iterator), "- Nie znaleziono nieunikalnych postów.", end='\n\n')
+	print("============= 1 - LICZBA POSTÓW ===================")
+	print(count_posts(user_post_iterator), end='\n\n')
 
-print("============= 3 - NAJBLIŻSI UŻYTKOWNICY ====================")
-[print(f"{k} jest najbliżej {v[1]} ({v[0]} km)") for k, v in find_closest(user_post_iterator).items()]
+	print("=========== 2 - NIEUNIKALNE TYTUŁY POSTÓW ============")
+	print(check_unique_title(user_post_iterator), "- Nie znaleziono nieunikalnych postów.", end='\n\n')
+
+	print("============= 3 - NAJBLIŻSI UŻYTKOWNICY ====================")
+	[print(f"{k} jest najbliżej {v[1]} ({v[0]} km)") for k, v in find_closest(user_post_iterator).items()]
 
 
 
